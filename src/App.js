9a316f4fab6368e0916bf58346eea52e6ef2c82f
Marta Soto Morrás <msotomorras@ideo.com>
2020-07-06 12:30:45 +0200

    test2 ga

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ShakerView from './app/ShakerView/ShakerView'
 import ReactGA from 'react-ga';
 import './App.scss';
 
-const App = () => {
+function initializeReactGA() {
   ReactGA.initialize('UA-171702799-1');
+  ReactGA.pageview('/');
+}
+
+const App = () => {
+  useEffect(() => {
+    initializeReactGA()
+  }, [])
   return (
     <div className="App">
       <ShakerView/>
